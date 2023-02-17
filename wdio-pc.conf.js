b66a186c8e6c64f97538200bf2ebf23b142bfa62
leonshane13 <seshio@163.com>
2023-02-17 16:30:54 +0800

    test

--- wdio-pc.conf.js
+++ wdio-pc.conf.js
@@ -188,10 +188,7 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec', 'cucumberjs-json',
-    ['junit', {
-        outputDir: './'
-    }]],
+    reporters: ['spec', 'cucumberjs-json'],
 
 
     //
